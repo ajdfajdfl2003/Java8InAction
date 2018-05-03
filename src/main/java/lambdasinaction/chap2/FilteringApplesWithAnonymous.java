4583725754e0be0320dfcdbb1bb2cfbec2434762
AngusWu <anguswu@simbo.com.tw>
2018-05-04 00:44:42 +0800

    Refactor: use lambda instead

--- src/main/java/lambdasinaction/chap2/FilteringApplesWithAnonymous.java
+++ src/main/java/lambdasinaction/chap2/FilteringApplesWithAnonymous.java
@@ -7,22 +7,17 @@ import java.util.List;
 public class FilteringApplesWithAnonymous {
 
     public static void main(String... args) {
-        List<Apple> inventory = Arrays.asList(new Apple(80, "green"), new Apple(155, "green"), new Apple(120, "red"));
+        List<Apple> inventory = Arrays.asList(
+                new Apple(80, "green"),
+                new Apple(155, "green"),
+                new Apple(120, "red"));
 
         // [Apple{color='green', weight=155}]
-        List<Apple> heavyApples = filter(inventory, new ApplePredicate() {
-            public boolean test(Apple apple) {
-                return apple.getWeight() > 150;
-            }
-        });
+        List<Apple> heavyApples = filter(inventory, apple -> apple.getWeight() > 150);
         System.out.println(heavyApples);
 
         // [Apple{color='red', weight=120}]
-        List<Apple> redApples2 = filter(inventory, new ApplePredicate() {
-            public boolean test(Apple apple) {
-                return apple.getColor().equals("red");
-            }
-        });
+        List<Apple> redApples2 = filter(inventory, apple -> apple.getColor().equals("red"));
         System.out.println(redApples2);
     }
 
@@ -37,19 +32,19 @@ public class FilteringApplesWithAnonymous {
     }
 
     public static class Apple {
-        private int weight = 0;
-        private String color = "";
+        private int weight;
+        private String color;
 
-        public Apple(int weight, String color) {
+        Apple(int weight, String color) {
             this.weight = weight;
             this.color = color;
         }
 
-        public Integer getWeight() {
+        Integer getWeight() {
             return weight;
         }
 
-        public String getColor() {
+        String getColor() {
             return color;
         }
 

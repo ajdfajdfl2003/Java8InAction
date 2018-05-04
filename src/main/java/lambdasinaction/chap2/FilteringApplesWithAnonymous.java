e79b11398509a059ba8618ad72ec36c063d39bbb
AngusWu <anguswu@simbo.com.tw>
2018-05-04 10:19:42 +0800

    using Generic and add Banana class for filter

--- src/main/java/lambdasinaction/chap2/FilteringApplesWithAnonymous.java
+++ src/main/java/lambdasinaction/chap2/FilteringApplesWithAnonymous.java
@@ -7,28 +7,75 @@ import java.util.List;
 public class FilteringApplesWithAnonymous {
 
     public static void main(String... args) {
-        List<Apple> inventory = Arrays.asList(
+        List<Apple> inventoryOfApple = Arrays.asList(
                 new Apple(80, "green"),
                 new Apple(155, "green"),
                 new Apple(120, "red"));
 
+        List<Banana> inventoryOfBanana = Arrays.asList(
+                new Banana(80, "yellow"),
+                new Banana(155, "black"),
+                new Banana(120, "black"));
+
         // [Apple{color='green', weight=155}]
-        List<Apple> heavyApples = filter(inventory, apple -> apple.getWeight() > 150);
+        List<Apple> heavyApples = filter(inventoryOfApple, apple -> apple.getWeight() > 150);
         System.out.println(heavyApples);
 
+        // [Banana{color='black', weight=155}]
+        List<Banana> heavyBanana = filter(inventoryOfBanana, banana -> banana.getWeight() > 150);
+        System.out.println(heavyBanana);
+
         // [Apple{color='red', weight=120}]
-        List<Apple> redApples2 = filter(inventory, apple -> apple.getColor().equals("red"));
-        System.out.println(redApples2);
+        List<Apple> redApples = filter(inventoryOfApple, apple -> apple.getColor().equals("red"));
+        System.out.println(redApples);
+        // [Banana{color='black', weight=155}, Banana{color='black', weight=120}]
+        List<Banana> blackBanana = filter(inventoryOfBanana, banana -> banana.getColor().equals("black"));
+        System.out.println(blackBanana);
     }
 
-    public static List<Apple> filter(List<Apple> inventory, ApplePredicate p) {
-        List<Apple> result = new ArrayList<>();
-        for (Apple apple : inventory) {
-            if (p.test(apple)) {
-                result.add(apple);
+    public interface Predicate<T> {
+        boolean test(T a);
+    }
+
+    /*
+        Java doesn't know what type of T is.
+        Until I instantiate a type
+        Because this is a static method,
+            I have to tell what type of this static method is
+    */
+    public static <T> List<T> filter(List<T> inventory, Predicate<T> p) {
+        List<T> result = new ArrayList<>();
+        for (T type : inventory) {
+            if (p.test(type)) {
+                result.add(type);
             }
         }
         return result;
+    }
+
+    public static class Banana {
+        private int weight;
+        private String color;
+
+        Banana(int weight, String color) {
+            this.weight = weight;
+            this.color = color;
+        }
+
+        Integer getWeight() {
+            return weight;
+        }
+
+        String getColor() {
+            return color;
+        }
+
+        public String toString() {
+            return "Banana{" +
+                    "color='" + color + '\'' +
+                    ", weight=" + weight +
+                    '}';
+        }
     }
 
     public static class Apple {
@@ -54,9 +101,5 @@ public class FilteringApplesWithAnonymous {
                     ", weight=" + weight +
                     '}';
         }
-    }
-
-    interface ApplePredicate {
-        boolean test(Apple a);
     }
 }

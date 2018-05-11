e2caf4b8dcad6841d476d7f32496d94911fa5e5e
AngusWu <anguswu@simbo.com.tw>
2018-05-11 11:49:06 +0800

    Use lambda and interface let code more flexiable

--- src/main/java/lambdasinaction/chap3/SelfDoExecuteAround.java
+++ src/main/java/lambdasinaction/chap3/SelfDoExecuteAround.java
@@ -8,14 +8,21 @@ public class SelfDoExecuteAround {
     private static ClassLoader classLoader = ExecuteAround.class.getClassLoader();
 
     public static void main(String... args) throws IOException {
-        String result = processFile();
-        System.out.println(result);
+        String oneLine = processFile((BufferedReader br) -> br.readLine());
+        System.out.println("This is one line:\n" + oneLine);
+
+        String twoLines = processFile((BufferedReader br) -> br.readLine() + br.readLine());
+        System.out.println("This is two line:\n" + twoLines);
     }
 
-    private static String processFile() throws IOException {
+    private static String processFile(BufferedReaderProcessor processor) throws IOException {
         try (BufferedReader reader = new BufferedReader(
                 new FileReader(classLoader.getResource("lambdasinaction/chap3/data.txt").getPath()))) {
-            return reader.readLine();
+            return processor.process(reader);
         }
+    }
+
+    interface BufferedReaderProcessor {
+        String process(BufferedReader reader) throws IOException;
     }
 }
